2ce79f5d24ab4adb58a4688e66741c8023020fc5
jacketex894 <jacketex894@gmail.com>
2016-09-29 23:49:42 +0800

    test2

--- index.php
+++ index.php
@@ -7,19 +7,10 @@
 </head>
 <body>
 <div id = "background"></div>
-<big><h1 style="color: 	#000000;">是食大友觀作低邊北龍</h1>
+<div id = "test"></div>
+<t1>LALALALA</t1>
 <strong><body text = 	#000000>
 <iframe width="640" height="360" src="https://www.youtube.com/embed/X85DcsKHaUo" frameborder="0" allowfullscreen></iframe>
-<p>
-<div id="ttt">
-<a id></a>
-<Table Width="300" Border="1">
-<th>
-拉拉拉<br>
-</strong>
-</th>
-</Table>
-</div>
-<img src="ner.gif">
+
 </body>
 </html>
