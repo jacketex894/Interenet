5eed700e733c17107f570d0ce66877f0b06dfca1
jacketex894 <jacketex894@gmail.com>
2016-10-10 22:00:09 +0800

    study

--- index.php
+++ index.php
@@ -4,26 +4,26 @@
 <link rel="shortcut icon" href="74A58PICMYF.ico" />
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="./index.css">
-    <link rel="stylesheet" type="text/css" href="./te.css">
-    <title>LALALALALALA</title>
+    <title>For Internet eduition</title>
 </head>
-<body>
-	<div id = "background"></div>
-	<div class="cube">
-  <div class="backlayer"></div>
-  <div class="frontlayer"></div>
-  <div class="rightlayer"></div>
-  <div class="leftlayer"></div>
-  <div class="toplayer"></div>
-  <div class="bottomlayer"></div>
-</div>
-		<div id = "inside">
-			<div id = "test">
-			<h1>LALALALA</h1>
-			</div>
-			<strong><body text = 	#000000>
-			<iframe width="640" height="360" src="https://www.youtube.com/embed/X85DcsKHaUo" frameborder="0" allowfullscreen></iframe>
-			<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D634901663357615%26set%3Da.562250760622706.1073741828.100005133601569%26type%3D3&width=500" width="500" height="391" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-		</div>
+
+<body background = "background.gif">
+	<div id = Table>
+	<table border = "1" cellpadding = "0" cellspacing = "0" bordercolor = "	#000000" width = "10%" align = "center" bgcolor="#000000">
+        <tr>
+        <td bgcolor="#FFCC00" height = "30"> &nbsp;</td>
+        <td bgcolor="#FFCC00" height = "30">Uva10061</td>
+        <td bgcolor="#FFCC00" height = "30">Uva10063</td>
+        <td bgcolor="#FFCC00" height = "30">Uva11344</td>
+        <td bgcolor="#FFCC00" height = "30">Uva10327</td>
+        <td bgcolor="#FFCC00" height = "30">Uva11000</td>
+        <tr>
+	</table>
+	<div id = study>
+	<a href="https://www.youtube.com/watch?v=EsbtE2DqUWg"><font size = +4 >哲學</font><p><a>
+	</div>
+    <a href = "https://zh.wikipedia.org/wiki/%E6%AF%94%E5%88%A9%C2%B7%E6%B5%B7%E7%81%B5%E9%A1%BF"><img src = "billi.jpg"></a>
+	</div>
+<script type="text/javascript" src="index.js"></script>
 </body>
 </html>
